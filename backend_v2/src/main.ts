5a02ed18f273ec8c8f04f88ff7d41047de5e46de
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2025-06-23 02:27:32 +0700

    Update main.ts

--- backend_v2/src/main.ts
+++ backend_v2/src/main.ts
@@ -34,9 +34,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config, {
-    include: [AppModule],
-  });
+  const document = SwaggerModule.createDocument(app, config, {});
   SwaggerModule.setup('api', app, document);
 
   // Start server
