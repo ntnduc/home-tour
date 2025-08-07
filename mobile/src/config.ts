c0ce0a06959f54e031faf59e9c5d30e02902ac02
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2025-08-07 22:25:54 +0700

    feat: cursor rule

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -1,3 +1,3 @@
-// export const API_URL = "http://192.168.12.125:3000";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "http://192.168.12.125:3000";
+// export const API_URL = "http://localhost:3000";
 export const PREFIX_URL = "/api";
