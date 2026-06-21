ac0c7b526dbe74f006b20eab98267850965a2638
Nguyễn Trần Nhật Đức <68223190+ntnduc@users.noreply.github.com>
2026-06-21 23:41:37 +0700

    Delete config.ts

--- mobile/src/config.ts
+++
@@ -1,5 +0,0 @@
-// export const API_URL = 'http://192.168.12.120:3000';
-// export const API_URL = 'http://192.168.16.50:3000';
-// export const API_URL = 'http://192.168.1.251:3000';
-export const API_URL = 'http://localhost:3000';
-export const PREFIX_URL = '/api';
